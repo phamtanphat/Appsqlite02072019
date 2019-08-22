9821f29312265e9796dac2526011b29e56d6cd50
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-08-22 19:17:34 +0700

    first commit

---
+++ app/src/main/java/com/example/sqlite02072019/db/SQLite.java
@@ -0,0 +1,38 @@
+package com.example.sqlite02072019.db;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.text.Spannable;
+
+public class SQLite extends SQLiteOpenHelper {
+
+    public SQLite(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+
+    // 1 : Câu lệnh truy vấn không trả về dữ liệu
+    public void onQuery(String sql){
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(sql);
+    }
+    //2 : Câu lệnh truy vấn trả về dữ liệu
+    public Cursor getData(String sql){
+        SQLiteDatabase db = getReadableDatabase();
+        return db.rawQuery(sql ,null);
+    }
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
