eac203554cf3ecb74feba2c837566d238f489556
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-08-22 19:31:07 +0700

    a

--- app/src/main/java/com/example/sqlite02072019/MainActivity.java
+++ app/src/main/java/com/example/sqlite02072019/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.sqlite02072019.db.SQLite;
+
 public class MainActivity extends AppCompatActivity {
 
+    SQLite sqLite;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //tao database
+        sqLite = new SQLite(this,"Quanlysinhvien.sql",null,1);
+
+        //tao table
+        String createTable = "CREATE TABLE IF NOT EXISTS Sinhvien(Id INTEGER PRIMARY KEY AUTOINCREMENT , Ten VARCHAR , Diachi VARCHAR , Namsinh INTEGER)";
     }
 }
