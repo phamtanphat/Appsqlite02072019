fa2708a3ed686a3336874b1b266128973e38df31
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-08-24 19:18:20 +0700

    a

--- app/src/main/java/com/example/sqlite02072019/MainActivity.java
+++ app/src/main/java/com/example/sqlite02072019/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.sqlite02072019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.sqlite02072019.db.SQLite;
 
@@ -15,13 +17,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //tao database
         sqLite = new SQLite(this,"Quanlysinhvien.sql",null,1);
-
         //tao table
         String createTable = "CREATE TABLE IF NOT EXISTS Sinhvien(Id INTEGER PRIMARY KEY AUTOINCREMENT , Ten VARCHAR , Diachi VARCHAR , Namsinh INTEGER)";
         sqLite.onQuery(createTable);
 
         //them du lieu
-        String insert = "INSERT INTO Sinhvien VALUES(null,'Nguyễn Văn A','Quận 1',1990)";
-        sqLite.onQuery(insert);
+//        String insert = "INSERT INTO Sinhvien VALUES(null,'Nguyễn Văn A','Quận 1',1990)";
+//        sqLite.onQuery(insert);
+
+        // lấy dữ liệu trong database
+//        String data = "SELECT Id , Ten ,Diachi ,Namsinh FROM Sinhvien";
+//        Cursor cursor = sqLite.getData(data);
+//        while (cursor.moveToNext()){
+//            int id = cursor.getInt(0);
+//            String ten = cursor.getString(1);
+//            String diachi = cursor.getString(2);
+//            int namsinh = cursor.getInt(3);
+//
+//            int indexcotdiachi = cursor.getColumnIndex("Diachi");
+//        }
     }
 }
