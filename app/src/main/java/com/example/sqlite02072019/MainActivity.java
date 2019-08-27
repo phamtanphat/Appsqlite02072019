11a168f2f1a4746b4a7c51454173d2af8d43d9dd
phatdroid94 <phatdroid94@gmail.com>
2019-08-27 18:36:55 +0700

    A

--- app/src/main/java/com/example/sqlite02072019/MainActivity.java
+++ app/src/main/java/com/example/sqlite02072019/MainActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.database.Cursor;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.sqlite02072019.db.SQLite;
 
@@ -17,23 +21,21 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     SinhvienAdapter msinhvienAdapter;
     ArrayList<Sinhvien> msinhviens;
+    Button btnReset,btnAdd;
+    EditText edtTen,edtDiachi,edtNamsinh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //tao database
         sqLite = new SQLite(this,"Quanlysinhvien.sql",null,1);
-        //tao table
-        String createTable = "CREATE TABLE IF NOT EXISTS Sinhvien(Id INTEGER PRIMARY KEY AUTOINCREMENT , Ten VARCHAR , Diachi VARCHAR , Namsinh INTEGER)";
-        sqLite.onQuery(createTable);
-
-        //them du lieu
-//        String insert = "INSERT INTO Sinhvien VALUES(null,'Nguyễn Văn A','Quận 1',1990)";
-//        sqLite.onQuery(insert);
-
-//         lấy dữ liệu trong database
 
         recyclerView = findViewById(R.id.recyclerviewSinhvien);
+        edtDiachi = findViewById(R.id.edittextDiachi);
+        edtTen = findViewById(R.id.edittextTen);
+        edtNamsinh= findViewById(R.id.edittextNamsinh);
+        btnAdd =findViewById(R.id.buttonThem);
+        btnReset = findViewById(R.id.buttonXoa);
         msinhviens = new ArrayList<>();
         msinhvienAdapter = new SinhvienAdapter(msinhviens);
         recyclerView.setAdapter(msinhvienAdapter);
@@ -49,5 +51,28 @@ public class MainActivity extends AppCompatActivity {
             msinhviens.add(new Sinhvien(id,ten,diachi,namsinh));
             msinhvienAdapter.notifyDataSetChanged();
         }
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edtDiachi.setText("");
+                edtNamsinh.setText("");
+                edtTen.setText("");
+            }
+        });
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String ten = edtTen.getText().toString();
+                String diachi = edtDiachi.getText().toString();
+                String namsinh =  edtNamsinh.getText().toString();
+
+                if (ten.length() <= 0 || diachi.length() <= 0 || namsinh.length()<=0) return;
+                String insert = String.format("INSERT INTO Sinhvien VALUES (null , '%s' , '%s' ,%d)" , ten,diachi,namsinh);
+                String insert = "INSERT INTO Sinhvien VALUES (null , ? , ? ,?)";
+
+            }
+        });
     }
+
+
 }
